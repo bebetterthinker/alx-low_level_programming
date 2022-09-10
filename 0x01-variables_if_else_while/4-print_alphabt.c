14baf3125f1e39d90dfb01f43b48687c77ebf925
www.bebetterthinker <www.thinkbetterthanyouguess@gmail.com>
2022-09-09 18:52:27 -0700

    Write a program that prints the alphab et in lowercase

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	if (letter != 'e' && letter != 'q')
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
