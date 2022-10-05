8ad83f5d29dab9f07e068a2231460979d99ec73e
www.bebetterthinker <www.thinkbetterthanyouguess@gmail.com>
2022-10-05 13:08:43 -0700

    Write a program that adds positive numbers.

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,37 +1,64 @@
-#include "main.h"
 #include <stdio.h>
-#include <tsdlib.h>
-#include <string.h>
-#include <ctype.h>
+#include <stdlib.h>
+#include "main.h"
+
+int _atoi(char *s);
 
 /**
- * main - Program that takes in all integer arguments and returns the sum
- * @argc: Number of command line arguments
- * @argv: Array name
+ * main - adds two strings, prints error if not digits in string
+ * @argc: argument count
+ * @argv: arguments
  *
- * Return: 1 if a non-integer is among the passed in arguments, 0 otherwise
+ * Return: 1 if error , 0 if no number, sum otherwise
  */
 int main(int argc, char *argv[])
 {
-	int  a = 0, i, j;
+	int a, i, sum = 0;
 
 	for (i = 1; i < argc; i++)
 	{
-		for (j = 0; argv[i][j]; j++)
+		a = _atoi(argv[i]);
+		if (argv[i][0] != '0' && a == 0)
 		{
-			if (isdigit(argv[i][j]) == 0)
-			{
-				puts("Error");
-				return (0);
-			}
+			printf("Error\n");
+			return (1);
+		}
+		sum += a;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
+
+/**
+ * _atoi - return a num inside of a string
+ * @s: pointer for string to parse
+ *
+ * Return: int pulled from string, 0 if none
+ */
+int _atoi(char *s)
+{
+	unsigned int num = 0;
+	int flag = -1, i = 0, neg = 1;
+
+	if (*s == '\0')
+		flag = 1;
+	for (i = 0; flag == -1; i++)
+	{
+		if ((*(s + i) >= '0') && (*(s + i) <= '9'))
+		{
+			if (num != 0)
+				num = ((num * 10) + (*(s + i) - '0'));
+			else
+				num = (*(s + i) - '0');
+		}
+		else
+		{
+			return (0);
+		}
+		if (*(s + i + 1) == '\0')
+		{
+			flag = 1;
 		}
 	}
-	for (i = 1; i < argc; i++)
-	{
-		a + = atoic(argv[i];
-	}
-
-	printf("%d\n", a);
-
-	return (0);
+	return (neg * num);
 }
